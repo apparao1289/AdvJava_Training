2fb196022c745dc511f7c6983578d5f41970f1ac
apparao <apparao1289@gmail.com>
2020-08-02 14:38:19 +0530

    Initial project setup

---
+++ jdbc/src/com/test/jdbc/EployeeTest.java
@@ -0,0 +1,30 @@
+package com.test.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class EployeeTest {
+
+	public static void main(String args[]) throws SQLException {
+		Connection con = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "oracle");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select eid,ename,job from employee");
+			while (rs.next()) {
+				System.out.println(rs.getInt(1) + " " + rs.getString(2) + "      " + rs.getString(3));
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+}
